3906f40fe5582cfcf352ce262be6b0c4a4da23f9
Indrianingsihputri <indrianingsihputri30@gmail.com>
2026-06-21 18:55:37 +0700

    DAL: Membuat method testInject

--- CRUDMahasiswaADO2/DAL.cs
+++ CRUDMahasiswaADO2/DAL.cs
@@ -148,5 +148,18 @@ namespace CRUDMahasiswaADO2
             SqlCommand cmdInsert = new SqlCommand(insertQuery, conn);
             cmdInsert.ExecuteNonQuery();
         }
+
+        public void testInject(string nim)
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            string query = "Update mahasiswa set nama = 'HACKED' where NIM = " + nim;
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.ExecuteNonQuery();
+        }
+
     }
 }
