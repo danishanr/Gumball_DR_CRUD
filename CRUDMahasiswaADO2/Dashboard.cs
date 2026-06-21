68648fd346afc0aba68e6fcce0d0cffb6d8c17bb
Indrianingsihputri <indrianingsihputri30@gmail.com>
2026-06-21 19:14:35 +0700

    Menambah kode di dalam konstruktor

--- CRUDMahasiswaADO2/Dashboard.cs
+++ CRUDMahasiswaADO2/Dashboard.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace CRUDMahasiswaADO2
 {
@@ -19,6 +20,29 @@ namespace CRUDMahasiswaADO2
         public Dashboard()
         {
             InitializeComponent();
+
+            dtpTanggalMasuk.MinDate = new DateTime(2000, 1, 1);
+            dtpTanggalMasuk.Format = DateTimePickerFormat.Custom;
+            dtpTanggalMasuk.CustomFormat = "yyyy";
+            dtpTanggalMasuk.ShowUpDown = true;
+            dtpTanggalMasuk.MaxDate = DateTime.Now;
+
+            cmbTipe.DropDownStyle = ComboBoxStyle.DropDownList;
+            var items = new List<KeyValuePair<string, SeriesChartType>>
+            {
+                new KeyValuePair<string, SeriesChartType>("Kolom", SeriesChartType.Column),
+                new KeyValuePair<string, SeriesChartType>("Pie", SeriesChartType.Pie)
+            };
+
+            isInitializing = true;
+
+            cmbTipe.DataSource = items;
+            cmbTipe.DisplayMember = "Key";
+            cmbTipe.ValueMember = "Value";
+            cmbTipe.SelectedIndex = 0;
+
+            isInitializing = false;
+            loadDataChart();
         }
 
         private void Dashboard_Load(object sender, EventArgs e)
