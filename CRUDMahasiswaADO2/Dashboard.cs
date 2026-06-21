8ddcae5d2bf859f836e472b9bbcb2b3b8d031d47
Indrianingsihputri <indrianingsihputri30@gmail.com>
2026-06-21 19:11:26 +0700

    Membuat Form Dashboard

---
+++ CRUDMahasiswaADO2/Dashboard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDMahasiswaADO2
+{
+    public partial class Dashboard : Form
+    {
+        DAL dbLogic = new DAL();
+        bool isInitializing = true;
+        DataTable dt;
+        int button = 0;
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void Dashboard_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
