5f83f81749f1ac5a3a12bcaa5d818d10a421eded
Indrianingsihputri <indrianingsihputri30@gmail.com>
2026-06-21 18:10:41 +0700

    DAL: Membuat method CountMhs

--- CRUDMahasiswaADO2/DAL.cs
+++ CRUDMahasiswaADO2/DAL.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace CRUDMahasiswaADO2
 {
-    internal class DAL
+    public class DAL
     {
+        static string connectionString ="Data Source=LAPTOP-49331NDM\\RIANIINDRI;Initial Catalog=DBAkademikADO;Integrated Security=True";
+
+        public string GetConnectionString()
+        {
+            return connectionString;
+        }
+
+        SqlConnection conn = new SqlConnection(connectionString);
+
+        SqlDataAdapter da;
+        DataTable dtMahasiswa;
+        DataTable dtProdi;
+
+        public int CountMhs()
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            SqlCommand cmd = new SqlCommand("sp_CountMahasiswa", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter outputParam = new SqlParameter("@pCount", SqlDbType.Int);
+            outputParam.Direction = ParameterDirection.Output;
+
+            cmd.Parameters.Add(outputParam);
+
+            cmd.ExecuteNonQuery();
+
+            return Convert.ToInt32(outputParam.Value);
+        }
     }
 }
